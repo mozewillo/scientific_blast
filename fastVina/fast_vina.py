464c9ac0752cc5d87ea76686a6c196f84ada5b0d
mozewillo <45209001+mozewillo@users.noreply.github.com>
2021-01-09 18:57:28 +0100

    add analysis part

--- fastVina/fast_vina.py
+++ fastVina/fast_vina.py
@@ -1,4 +1,4 @@
-"""Running vina docking on a group of ligands """
+"""Running vina docking on a group of ligands"""
 
 # Script using the AutoDock Vina more information below
 # O. Trott, A. J. Olson,                                        
@@ -14,6 +14,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import subprocess
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import spearmanr
+from scipy.stats import pearsonr
 from time import time
 
 parser=argparse.ArgumentParser(description='Specify the parameters to run multiple docking.')
@@ -28,6 +32,7 @@ parser.add_argument('-sz','--size_z', type=float, required=True, help="size in t
 parser.add_argument('-e','--exhaustiveness', type=int, required=False, help="exhaustiveness of the global search", default=8)
 parser.add_argument('-n','--num_modes', type=int, required=False, help="maximum number of binding modes to generate", default=9)
 parser.add_argument('-seed','--seed', type=int, required=False, help="random seed", default=123)
+parser.add_argument('-a','--a', type=int, required=False, help="short analysis on the docking results")
 args = parser.parse_args()
 
 
@@ -40,7 +45,6 @@ if __name__ == '__main__':
 		out = out.split()[2]
 	except subprocess.CalledProcessError:
 		print("Couldn't find vina :(")
-
 
 	# run docking in given directory
 	if os.path.isdir(args.dir):
@@ -59,3 +63,42 @@ if __name__ == '__main__':
 			print(f'Docking number : {num} finished with time : {s_delta}')
 		delta = end - start
 		print(f'Docking finished. Docking time: {delta}')
+
+	if args.a:
+		# run short analyses / Required: file names should follow a pattern name-<IC>.pdbqt
+		names = []
+		ic = []
+		scores = []
+		if os.path.isdir(args.dir):
+			files = [file for file in os.listdir(args.dir) if file.endswith('out.pdbqt')]
+			for file in files:
+				file = args.dir + "/" + file
+				print(file)
+				with open(file,'r') as f :
+				    lines = f.readlines()
+				    for l in lines:
+				    	if l.startswith('REMARK VINA RESULT'):
+				    		max_val = l.split()[3]
+				    		max_val = float(max_val)
+				    		scores.append(max_val)
+				    	if l.startswith('REMARK  Name = '):
+				    		IC_val = l.split()[3]
+				    		IC_val = IC_val.split('-')[1]
+				    		IC_val = float(IC_val)
+				    		ic.append(IC_val)
+				    		break
+			dict = {'Experimental': ic, 'Theoretical':scores}
+			df = pd.DataFrame(dict)
+			print(df)
+			# calculate correlations
+			corr_p, pval_p = pearsonr(df['Experimental'],df['Theoretical'])
+			corr_s, pval_s = spearmanr(df['Experimental'],df['Theoretical'])
+			print(f'Spearman correlation {corr_s} with p-value {pval_s}')
+			print(f'Pearson correlation {corr_p} with p-value {pval_p}')
+			out = args.dir + "/" + 'scores'
+			df.to_csv(out)
+			# plot the results
+			plt.scatter(df['Experimental'],df['Theoretical'])
+			plt.xlabel("Experimental")
+			plt.ylabel("Theoretical")
+			plt.show()
